ec2fb2fc5485e75bdbd2f863de58d6b9992b38d8
drupchen <hhdrupchen@gmail.com>
2018-02-05 17:14:43 +0100

    bugfix: vowel sandhis and visarga1 sandhis with a I initial

--- sandhi_engine.py
+++ sandhi_engine.py
@@ -74,5 +74,5 @@ if __name__ == "__main__":
         initial = 'asti'
         for c in currents:
             print('{} + {} =>'.format(c, initial), ', '.join(engine.apply_sandhi(c, initial)))
-        c, initial = 'pAhi', 'evam'
+        c, initial = 'tattva', 'Ikzina'
         print('{} + {} =>'.format(c, initial), ', '.join(engine.apply_sandhi(c, initial)))
